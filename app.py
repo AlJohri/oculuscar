2d21bd531ca65aa00e2d009b521f956c56064a59
Al Johri <al.johri@gmail.com>
2015-04-18 21:14:15 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+last_data = {}
+
+@app.route('/', methods = ['POST'])
+def test():
+	last_data = dict(request.form)
+	return str(last_data)
+
+@app.route('/', methods = ['GET'])
+def other_function():
+	return str(last_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.getenv('port', '5000')), host='0.0.0.0')
