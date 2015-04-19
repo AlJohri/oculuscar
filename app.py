629965b95ee4c018fe388fdcf9518bd400b949e0
janeelke <janemiller2012@u.northwestern.edu>
2015-04-19 08:57:16 -0500

    fixed f
    fixed f

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-last_data = {}
+global last_data = dict([['servo', 0], ['left', 0], ['right', 0]])
 
 @app.route('/', methods = ['POST'])
 def test():
-	global last_data
-	last_data = dict(request.form)
+	temp = dict(request.form)
+	if 'left' in temp:
+		last_data['left'] = temp['left']
+	if 'right' in temp:
+		last_data['right'] = temp['right']
+	if 'servo' in temp:
+		last_data['servo'] = temp['servo']
 	return str(last_data)
 
 @app.route('/', methods = ['GET'])
